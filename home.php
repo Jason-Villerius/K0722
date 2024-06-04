568b4b60a6f15b290f4ecce779cef7eb89b74780
Jason-Villerius <9021149@student.zadkine.nl>
2024-06-04 12:23:18 +0200

    links

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="style.css" rel="stylesheet">
+    <link href="styles.css" rel="stylesheet">
     <title>Mijn Portfolio</title>
 </head>
 <body>
@@ -34,9 +34,16 @@
         </section>
     </main>
     <footer>
+    <section>
     <p>Contactgegevens:</p>
     <p>Telefoonnummer: +31 6 49133297</p>
     <p>E-mailadres: jasonvillerius@gmail.com</p>
+    </section>
+    <section>
+    <p>Mijn Links:</p>
+    <p>LinkedIn:  <a href="https://www.linkedin.com/in/jason-villerius-9227952a0/"> LinkedIn profile</a></p>
+    <p>Github: <a href="https://github.com/Jason-Villerius"> Github profile</a></p>
+    </section>
     </footer>
 </body>
 </html>
